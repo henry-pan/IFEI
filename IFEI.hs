db1f3d575819994e7178003b1fb115b5ac3e3bb7
Henry Pan <henry-pan@users.noreply.github.com>
2016-02-08 00:24:34 -0800

    Game Loading

--- IFEI.hs
+++ IFEI.hs
@@ -3,11 +3,11 @@ import System.IO
 --Asks for your name. Reads text file.
 --fileLines is a list of the lines in text file. 
 main = do
-    putStrLn $ "Welcome to this text adventure. What's your name?"
+    putStrLn $ "Please load a game by typing in the name of the game file, without the file extension: "
     name <- getLine
-    putStrLn $ "Hi " ++ name
-    handle <- openFile "text.txt" ReadMode
+    handle <- openFile (name ++ ".txt") ReadMode
     contents <- hGetContents handle
+    putStrLn $ "\n" ++ name ++ " loaded.\n"
     let fileLines = lines contents
     let command = getCommand fileLines
     putStrLn $ command
@@ -15,14 +15,15 @@ main = do
 
 --Gets first word from string line.
 getFirstWord :: String -> String
-getFirstWord []= ""
+getFirstWord [] = ""
 getFirstWord x = head (words x)
 
 --Gets lists of all the lines from input file, and does something depending on first word of string.
 --BUG: Can't get it to recurse through the entire list, so for now it just reads the first item in list.
 getCommand :: [String] -> String
-getCommand []= ""
+getCommand [] = ""
 getCommand (c:cs)
-   |x=="makeRoom" = "Insert room function here"
-   |x=="option" = "Insert options function here"
-   where x = getFirstWord c
+    |x == "makeRoom" = "Insert room function here"
+    |x == "option"   = "Insert options function here"
+    where x = getFirstWord c
+   
