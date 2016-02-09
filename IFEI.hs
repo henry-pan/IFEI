53be3909b892cce9fca13912ce22eab2ba5530de
Henry Pan <henry-pan@users.noreply.github.com>
2016-02-09 01:39:19 -0800

    Handler for invalid files

--- IFEI.hs
+++ IFEI.hs
@@ -4,38 +4,27 @@
 -- Authors: Steven Mazliach, Kavya Rammohan, Henry Pan, Thais Aoki.
 -----------------------------------------------------------------------
 import System.IO
+import System.Directory
 
 --Reads from a text file.
 --fileLines is a list of the lines in text file. 
 main = do
-    putStrLn $ "Please load a game by typing in the name of the game file, without the file extension: "
+    putStrLn "Please load a game by typing in the name of the game file, without the file extension: "
     name <- getLine
-    handle <- openFile (name ++ ".txt") ReadMode
-    contents <- hGetContents handle
-    putStrLn $ "\n" ++ name ++ " loaded.\n"
-    let fileLines = lines contents
-    let command = getCommand fileLines
-    putStrLn $ command
-    hClose handle
+    validFile <- doesFileExist (name ++ ".txt")
+    if validFile
+        then do
+            handle <- openFile (name ++ ".txt") ReadMode
+            contents <- hGetContents handle
+            putStrLn $ "\n" ++ name ++ " loaded.\n"
+            let fileLines = lines contents
+            let command = getCommand fileLines
+            putStrLn $ command
+            hClose handle
+        else do
+            putStrLn "\nThe file does not exist.\n"
+            main
 
---Gets first word from string line.
-getFirstWord :: String -> String
-getFirstWord [] = ""
-getFirstWord x = head (words x)
-
---Gets lists of all the lines from input file, and does something depending on first word of string.
---BUG: Can't get it to recurse through the entire list, so for now it just reads the first item in list.
---Change this to createRooms, which will take the words of the input file and turn it into a list of Rooms.
-getCommand :: [String] -> String
-getCommand [] = ""
-getCommand (c:cs)
-    | x == "[Room" = printRoom (c,cs)
-    | x == "[Path" = "Insert command function here"
-    where x = getFirstWord c
-
-    
-    
-    
 --Int: Room ID. Identifies the room.
 --String: Text data associated with the room.
 --[Path]: The list of paths that can be taken from the room.
@@ -50,7 +39,7 @@ We might want to create some sort of data structure to hold all the
 room and path data. List of Tuples?
 So the end result would be something like but dynamically generated:
 
-gameData = [(Room0,[Paths]) , (Room1,[Paths]), (Room3,[Paths])]
+gameData = [(Room0,[Paths0]) , (Room1,[Paths1]), (Room2,[Paths2])]
 
 
 If we do this, we can easily grab a room using list indices.
@@ -58,6 +47,28 @@ We will need to implement some sort of exception handling in case
 the user decides to number rooms out of order.
 
 -}
+
+
+
+--Gets first word from string line.
+getFirstWord :: String -> String
+getFirstWord [] = ""
+getFirstWord x = head (words x)
+
+--Gets lists of all the lines from input file, and does something depending on first word of string.
+--BUG: Can't get it to recurse through the entire list, so for now it just reads the first item in list.
+--Merge with createRooms?
+getCommand :: [String] -> String
+getCommand [] = ""
+getCommand (c:cs)
+    | x == "[Room" = printRoom (c,cs)
+    | x == "[Path" = "Insert command function here"
+    where x = getFirstWord c
+
+    
+--Take the words of the input file and turn it into a list of Rooms.
+--createRooms :: [String] -> [Room]
+    
     
 --Prints a Room and its associated commands.
 --(Text Data,[Commands]) -> Printed Room
