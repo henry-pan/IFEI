67880755b27f1ef620200c61721e3651799b3f36
Henry Pan <henry-pan@users.noreply.github.com>
2016-02-13 02:20:58 -0800

    Update

--- IFEI.hs
+++ IFEI.hs
@@ -20,16 +20,20 @@ main = do
             handle <- openFile (name ++ ".txt") ReadMode
             contents <- hGetContents handle
             
+            
+            --Fancy ASCII display.
             putStrLn $ "\n==" ++ (replicate (length (name ++ " loaded.")) '=') ++ "=="
             putStrLn $ "| " ++ name ++ " loaded. |"
             putStrLn $ "==" ++ (replicate (length (name ++ " loaded.")) '=') ++ "==\n"
             
+            
             putStrLn $ unwords $ splitFile contents
             --let fileLines = lines contents
-            --putStrLn $ getCommand fileLines
+            --putStrLn $ createRooms fileLines
             game
             hClose handle
         else do
+            --Fancy ASCII display.
             putStrLn "\n============================"
             putStrLn "| The file does not exist. |"
             putStrLn "============================\n"
@@ -74,7 +78,7 @@ We might want to create some sort of data structure to hold all the
 room and path data. List of Tuples?
 So the end result would be something like but dynamically generated:
 
-gameData = [(Room0,[Paths0]) , (Room1,[Paths1]), (Room2,[Paths2])]
+gameData = [(Room0,[Paths0]) , (Room1,[Paths1]), (Room2,[Paths2]),...]
 
 
 If we do this, we can easily grab a room using list indices.
@@ -85,39 +89,48 @@ the user decides to number rooms out of order.
 --startsWith, from Data.List.Split
 --https://hackage.haskell.org/package/split-0.1.1/docs/Data-List-Split.html
 --Take the input file and turn it into a list of Strings (incomplete Rooms).
---Technically could be condensed into one line, but kept separate for clarity.
+--Technically could be condensed into one line in the I/O section, but kept separate for clarity.
 splitFile :: String -> [String]
 splitFile = split (startsWith "[Room")
 
 
+--Helper function.
+--May or may not be needed.
 --Gets first word from string line.
 getFirstWord :: String -> String
 getFirstWord [] = ""
 getFirstWord x = head (words x)
 
 
---Gets lists of all the lines from input file, and does something depending on first word of string.
---BUG: Can't get it to recurse through the entire list, so for now it just reads the first item in list.
---Merge with createRooms?
-getCommand :: [String] -> String
-getCommand [] = ""
-getCommand (c:cs)
+
+--createRooms :: [String] -> [(Room,[Path])]
+--Take divided file and turn it into gameData
+--Old code. Please edit this.
+createRooms :: [String] -> String
+createRooms [] = ""
+createRooms (c:cs)
     | x == "[Room" = printRoom (c,cs)
     | x == "[Path" = "Insert command function here"
     where x = getFirstWord c
     
---createRooms
---Take divided file and turn it into gameData
---createRooms :: [String] -> [(Room,[Path])]
+
 
 --processCommand
 --Take input command and check paths for commmand
 --If input and a path match, take that path.
+--Path ID == Room ID == Destination Room
 --processCommand :: String -> [Path] -> ???
 
-    
---Prints a Room and its associated commands.
---(Text Data,[Commands]) -> Printed Room
---Temporary!
+
+
+--Helper function to printRoom.
+--Takes paths and turns them into a string to display alongside the room text.
+--printPath :: [Path] -> String
+
+
+
+--Prints a room and its associated commands.
+--printRoom :: (Room,[Path]) -> String
+--Temporary: (Text Data,[Commands]) -> Printed Room
 printRoom :: (String,[String]) -> String
 printRoom (x,y) = unlines [x] ++ unwords y
