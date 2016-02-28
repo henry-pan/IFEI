523cc94ebd6397cce1da830ab6e1e0020f8412b6
thaisaoki <taoki@ucsc.edu>
2016-02-28 11:44:21 -0800

    made changes to look more like the Haskell version.

--- IFEI.java
+++ IFEI.java
@@ -9,8 +9,25 @@ class IFEI{
 
     public static void main (String[] args) throws IOException{
     	//Reads from a text file
+    	System.out.println(">>> Please load a game by typing in the name of the game file, without the file extension: ");
+    	Scanner stdin = new Scanner (in);
+    	String file= stdin.nextLine();
+    	String fi=file+".txt";
+    	File f= new File(fi);
+    	if(!f.exists()){
+    		System.out.println("\n============================");
+    		System.out.println("| The file does not exist  |");
+    		System.out.println("============================");
+    		System.exit(0);
+    	}
+    	else{
+    		String eq="===============================================================";
+    		System.out.println("\n=" + eq.substring(0,(file + " loaded  |").length()) + "=");
+    		System.out.println("| " +file + " loaded  |");
+    		System.out.println("=" + eq.substring(0,(file + " loaded  |").length()) + "=");
+    	}
     	FileReader readingFile= null;
-        readingFile= new FileReader(args[0]);
+        readingFile= new FileReader(fi);
         BufferedReader br = (new BufferedReader(readingFile));
         String line= null;
         
@@ -34,17 +51,12 @@ class IFEI{
             //Gets description from room.
             else if(line != null){
             	insideRoom.insertDescription(line);
-            }else{
-            	System.out.println("============================");
-            	System.out.println("| The file does not exist. |");
-            	System.out.println("============================");
             }
         }
         //Displays the first room.
         insideRoom.displayStart();
         boolean readFile= true;
         while(readFile == true){
-            Scanner stdin = new Scanner (in);
             for (;;) {
                 if (! stdin.hasNextLine()) break;
                 String inputline = stdin.nextLine();
@@ -52,22 +64,33 @@ class IFEI{
                 String command = inputline.toLowerCase();
                 if (command.equals("exit")){
                 	readFile =false;
-                	System.out.println("Goodbye.");
-                	break;
+                	System.out.println("\n>>> Are you sure you want to exit the game? Type 'exit' again to quit.");
+                	if(((stdin.nextLine()).toLowerCase()).equals("exit")){
+                		System.out.println("\n>>> Goodbye.");
+                		break;
+                	}else{
+                		System.out.println("\n>>> Cancelled.");
+                	}
                 }
                 //Displays every possible command, including reserved commands.
                 else if (command.equals("help")){
-                	System.out.println("================");
+                	System.out.println("\n================");
                 	System.out.println("| Command List |");
                 	System.out.println("================");
-                	System.out.println("Help\nRepeat\nRestart\nExit\n");
+                	System.out.println("Help\nRepeat\nRestart\nExit");
+                	insideRoom.displayOpts();
                 }
                 //Displays the first room of the game.
                 else if(command.equals("restart")){
-                	System.out.println("================");
-                	System.out.println("| Restarting...|");
-                	System.out.println("================");
-                	insideRoom.displayStart();
+                	System.out.println("\n>>> Are you sure you want to restart the game? Type 'restart' again to quit.");
+                	if(((stdin.nextLine()).toLowerCase()).equals("restart")){
+                		System.out.println("\n================");
+                		System.out.println("| Restarting...|");
+                		System.out.println("================");
+                		insideRoom.displayStart();
+                	}else{
+                		System.out.println("\n>>> Cancelled.");
+                	}
                 }
                 //Displays the contents of the current room again.
                 else if(command.equals("repeat")){
